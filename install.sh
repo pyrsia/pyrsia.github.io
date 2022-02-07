789084308beff13abed7853ef942b67b123d30b7
Steve Taylor <steve@deployhub.com>
2022-02-07 10:22:37 -0700

    point to focal instead of bionic for repo os

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 apt-get update
 apt-get install -y wget gnupg
 wget -qO - https://pyrsia.io/public.key | apt-key add -
-echo "deb https://pyrsia.io/repo bionic main" >> /etc/apt/sources.list
+echo "deb https://pyrsia.io/repo focal main" >> /etc/apt/sources.list
 apt-get update
 apt-get install -y pyrsia
