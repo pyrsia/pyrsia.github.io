448c5eef720eeacd99d1f6632c3254f83b280a59
Markus Binder <37409811+markus-jf@users.noreply.github.com>
2022-05-08 12:56:26 +0300

    MAR-4898 Fix canonical meta tags

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Pyrsia.io',
   tagline: 'Decentralized Package Network',
-  url: 'https://pyrsia.io/',
+  url: 'https://pyrsia.io',
   baseUrl: '/',
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
