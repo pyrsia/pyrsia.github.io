b514ae7012fc632db54dbd5f2997979e6eb37a6a
Steve Taylor <steve@deployhub.com>
2022-11-17 09:47:20 -0700

    add missing install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get install -y wget gnupg
+wget -q -O - https://repo.pyrsia.io/repos/Release.key |  gpg --dearmor  > pyrsia.gpg
+install -o root -g root -m 644 pyrsia.gpg /etc/apt/trusted.gpg.d/
+rm pyrsia.gpg
+echo "deb https://repo.pyrsia.io/repos/nightly focal main" >> /etc/apt/sources.list
+apt-get update
+apt-get install -y pyrsia
