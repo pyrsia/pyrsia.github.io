237fecc825753f48d79a8cf1fa0e54df7c61f43e
Stephen Chin <steveonjava@gmail.com>
2023-11-02 18:29:21 -0700

    Remove companies from homepage (#190)

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,7 +54,6 @@ export default function Home() {
         <HomepageFeatures />
         <HomepagePkgFeatures />
         <HomepageSeparator />
-        <HomepageTeam />
       </main>
     </Layout>
   );
